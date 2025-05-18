3695d255f8bea5254522689cf1b40f909fa52ae8
King-Darshan-Rawan <darshanmalviya9826@gmail.com>
2025-05-18 17:25:26 +0530

    elements

--- responsiveFront/src/components/ElementPalette.jsx
+++ responsiveFront/src/components/ElementPalette.jsx
@@ -1,24 +1,31 @@
-const elements = [
-  { type: 'text', label: 'Text' },
-  { type: 'image', label: 'Image' },
-  { type: 'button', label: 'Button' },
-];
+import { nanoid } from 'nanoid';
 
-export default function ElementPalette() {
+export default function ElementPalette({ setElements }) {
+  const addElement = (type) => {
+    const newElement = {
+      id: nanoid(),
+      type,
+      content: type === 'text' ? 'Sample Text' : '',
+      label: type === 'button' ? 'Click Me' : '',
+      src: type === 'image' ? 'https://via.placeholder.com/150' : '',
+      x: 50,
+      y: 50,
+    };
+    setElements((prev) => [...prev, newElement]);
+  };
+
   return (
-    <div className="p-3 bg-light border-end h-100">
+    <div className="p-3">
       <h5>Elements</h5>
-      {elements.map((el, idx) => (
-        <div
-          key={idx}
-          draggable
-          onDragStart={(e) => e.dataTransfer.setData('type', el.type)}
-          className="p-2 border rounded mb-2 bg-white text-center cursor-pointer"
-        >
-          {el.label}
-        </div>
-      ))}
+      <button className="btn btn-light w-100 my-2" onClick={() => addElement('text')}>
+        Text
+      </button>
+      <button className="btn btn-light w-100 my-2" onClick={() => addElement('image')}>
+        Image
+      </button>
+      <button className="btn btn-light w-100 my-2" onClick={() => addElement('button')}>
+        Button
+      </button>
     </div>
   );
 }
-// This code defines a React component called `ElementPalette` that renders a list of draggable elements (text, image, and button) in a sidebar. Each element is represented by a div with a label and can be dragged to a different location in the application. The component uses Bootstrap classes for styling.
